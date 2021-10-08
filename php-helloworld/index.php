b922a3fdd05ccbec89b6bf0166d9729a29dac0f8
Dario Mora <d_mora@cooperativauniversitaria.coop>
2021-10-08 10:53:00 -0300

    se crea un nuevo archivo y se elimina la lina para solucionar conflicto

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -5,5 +5,4 @@
     echo "se crea la rama nueva-rama-desa";
     echo "nueva linea agregada a nueva-rama-desa luego del merge";
     echo "linea agregada a rama-desa luego del primer merge";
-    echo "linea agregada en nueva-rama-desa, luego de crear el archivo en rama-desa";
 ?>
